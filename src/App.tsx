4879708f4f636f1b04a09e07b718f462c5c49101
EnzoCerqueira <enzo.cerqueira42@gmail.com>
2026-04-12 16:37:19 -0300

    training styles and changing him

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import { FaSearch } from "react-icons/fa";
+import { CiLight } from "react-icons/ci";
 
 function App() {
   return (
@@ -6,16 +8,23 @@ function App() {
       <div className="content">
         <header>
           <h1>DevFinder</h1>
-          <button>light</button>
+          <div className="fds">
+            <button>light</button>
+            <CiLight className="light-icon" />
+          </div>
         </header>
         <div className="search-container">
+          <FaSearch />
           <input type="search" placeholder="Search GitHub username..." />
           <button>Search</button>
         </div>
         <div className="search-results">
           <div className="user-profile-info">
-            <h2>GitHub UserName</h2>
-            <p>Joined 2026</p>
+            <div className="search-results-header">
+              <img src="https://via.placeholder.com/150" alt="User Avatar" />
+              <h3>GitHub UserName</h3>
+              <p>Joined 2026</p>
+            </div>
             <h6>@username</h6>
             <p>Bio</p>
           </div>
@@ -33,17 +42,17 @@ function App() {
               <p>12</p>
             </div>
           </div>
-          <div className="user-extra-infos">
-            <div className="user-extra-info">
+          <div className="user-infos">
+            <div className="user-infos-item">
               <p>📍Location</p>
             </div>
-            <div className="user-extra-info">
-              <p>📎link</p>
-            </div>
-            <div className="user-extra-info">
+            <div className="user-infos-item">
               <p>🐦Twitter</p>
             </div>
-            <div className="user-extra-info">
+            <div className="user-infos-item">
+              <p>📎link</p>
+            </div>
+            <div className="user-infos-item">
               <p>🏢Company</p>
             </div>
           </div>
